08c3cea6ff6f4e046b019d44bfb78fb0475bc9d6
Eric Evans <eevans@wikimedia.org>
2016-09-23 15:50:58 -0500

    c-foreach-nt: reimplemented in Python

--- cassandra/tools/commands.py
+++ cassandra/tools/commands.py
@@ -1,6 +1,8 @@
 
 import logging
-import subprocess
+import sys
+
+from subprocess import Popen, PIPE
 
 from .config import DRY_RUN
 
@@ -13,19 +15,32 @@ def __ansi_color(code):
         return "\033[%sm%s\033[0m" % (c, text)
     return inner
 
+def __no_color(output):
+    return output
 
-RED     = __ansi_color("31")
-GREEN   = __ansi_color("32")
-YELLOW  = __ansi_color("33")
-BLUE    = __ansi_color("34")
-MAGENTA = __ansi_color("35")
-CYAN    = __ansi_color("36")
-WHITE   = __ansi_color("37")
+red     = __ansi_color("31")
+green   = __ansi_color("32")
+yellow  = __ansi_color("33")
+blue    = __ansi_color("34")
+magenta = __ansi_color("35")
+cyan    = __ansi_color("36")
+white   = __ansi_color("37")
 
 
 def call(*args, **kwargs):
+    color = kwargs.get("color", __no_color)
+
     if DRY_RUN:
         logging.info("%s (dry-run)", " ".join(list(args)))
         return 0
     else:
-        return subprocess.call(list(args))
+        process = Popen(args, stdout=PIPE, stderr=PIPE)
+
+        (stdout, stderr) = process.communicate()
+
+        if stdout:
+            sys.stdout.write(color(stdout))
+        if stderr:
+            sys.stderr.write(color(stderr, True))
+
+        return process.returncode
